ddc432ccb7bf33133bb5b1c0a05f6bd7d404b2ac
Egorka_Miller <ikuskov4@gmail.com>
2021-10-16 12:43:11 +0300

    Добавил логирование в проект и повесил атрибут [Authorize] на контроллер

--- FeedbackSchool/Controllers/HomeController.cs
+++ FeedbackSchool/Controllers/HomeController.cs
@@ -3,20 +3,16 @@ using System.Threading.Tasks;
 using FeedbackSchool.Data;
 using FeedbackSchool.Data.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using FeedbackSchool.Models;
 
 namespace FeedbackSchool.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
         private readonly IRepository<Guest, FeedbackModel> _repository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
             _repository = new EfRepository();
         }
 
--- FeedbackSchool/Controllers/ManageController.cs
+++ FeedbackSchool/Controllers/ManageController.cs
@@ -1,23 +1,33 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using FeedbackSchool.Areas.Identity.Data;
 using FeedbackSchool.Data;
 using FeedbackSchool.Data.EntityFramework;
 using FeedbackSchool.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace FeedbackSchool.Controllers
 {
+    [Authorize]
     public class ManageController : Controller
     {
         private readonly IRepository<Guest, FeedbackModel> _repository;
         private readonly RedirectToActionResult _redirectToAction;
+        private readonly UserManager<FeedbackSchoolUser> _userManager;
+        private readonly ILogger<ManageController> _logger;
 
-        public ManageController()
+        public ManageController(UserManager<FeedbackSchoolUser> userManager, ILogger<ManageController> logger)
         {
             _repository = new EfRepository();
-            
+
             _redirectToAction = RedirectToAction(nameof(Index));
+            _userManager = userManager;
+            _logger = logger;
         }
 
         // GET
@@ -29,12 +39,12 @@ namespace FeedbackSchool.Controllers
         public async Task<IActionResult> Index(string feedbacks)
         {
             // потом мб придумаю что-то лучше
-            
-            ViewResult view = null;
 
+            ViewResult view;
+
+            var count = 0;
             if (ModelState.IsValid)
             {
-                var count = 0;
                 try
                 {
                     foreach (var feedbackNumber in feedbacks.Split(','))
@@ -42,6 +52,8 @@ namespace FeedbackSchool.Controllers
                         await _repository.DeleteFeedback(int.Parse(feedbackNumber));
                         count++;
                     }
+
+                    view = View(_repository);
                 }
                 catch (FormatException e)
                 {
@@ -53,6 +65,8 @@ namespace FeedbackSchool.Controllers
             else
                 view = View(_repository);
 
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} удалил {count} отзыв(-ов)");
+
             return view;
         }
 
@@ -61,6 +75,9 @@ namespace FeedbackSchool.Controllers
             await System.IO.File.WriteAllTextAsync("DataBase.json",
                 JsonConvert.SerializeObject(_repository.GetAllList()));
             Response.Headers.Add("Content-Disposition", "attachment; filename=DataBase.json");
+
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} скачал базу даных");
+
             return new FileContentResult(await System.IO.File.ReadAllBytesAsync("DataBase.json"), "application/json");
         }
 
@@ -70,6 +87,8 @@ namespace FeedbackSchool.Controllers
         {
             await _repository.DeleteAllFeedback();
 
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} удалил все отзывы");
+
             return _redirectToAction;
         }
 
@@ -77,11 +96,18 @@ namespace FeedbackSchool.Controllers
         {
             await _repository.AddSchool(new FeedbackModel() {School = addSchool});
 
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} добавил школу {addSchool}");
+
             return _redirectToAction;
         }
 
         public async Task<IActionResult> DeleteSchool(string id)
         {
+            var school = _repository.GetSchoolClass().FirstOrDefault(f => f.Id == id)?.School;
+
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} удалил школу {school}");
+            
+            
             await _repository.DeleteSchool(new FeedbackModel() {Id = id});
 
             return _redirectToAction;
@@ -91,13 +117,20 @@ namespace FeedbackSchool.Controllers
         {
             await _repository.AddClass(new FeedbackModel() {Class = addClass});
 
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} добавил {addClass} класс ");
+
             return _redirectToAction;
         }
 
         public async Task<IActionResult> DeleteClass(string id)
         {
-            await _repository.DeleteClass(new FeedbackModel() {Id = id});
+            var classes = _repository.GetSchoolClass().FirstOrDefault(f => f.Id == id)?.Class;
 
+            _logger.LogInformation($"Пользователь {_userManager.GetUserName(User)} удалил {classes} класс");
+
+            
+            await _repository.DeleteClass(new FeedbackModel() {Id = id});
+            
             return _redirectToAction;
         }
 
