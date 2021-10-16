ba099b6bc087c4de7c74a858b465886b2a3d1a93
Egorka_Miller <ikuskov4@gmail.com>
2021-10-16 15:08:49 +0300

    Поменял логирование на Serilog

--- FeedbackSchool/Startup.cs
+++ FeedbackSchool/Startup.cs
@@ -6,13 +6,13 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace FeedbackSchool
 {
     public class Startup
     {
         public static string Connection;
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +26,11 @@ namespace FeedbackSchool
         {
             services.AddTransient<IdentityErrorDescriber, RussianIdentityErrorDescriber>();
             services.AddTransient<IEmailSender, SendMail>();
+            services.AddSingleton<ILogger>(_ => 
+                new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt")
+                .CreateLogger());
             services.AddControllersWithViews();
         }
 
